8a1c80a97f2eb5721c2be64e99792906eee464ba
root <susmi.chava@gmail.com>
2021-07-15 12:13:30 +0000

    react js intro

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="background">
+    <h1>Super Over League</h1>
+    <div class="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="W-100"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="W-100"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
